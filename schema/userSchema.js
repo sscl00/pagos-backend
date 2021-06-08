6d9f47772a83c4d71dfd3444e1f1b78b4faeab2f
scl <sofia.colin@ingenieria.unam.edu>
2021-06-08 17:50:31 -0500

    the user's schema was modified: date and email. @joi/date

--- schema/userSchema.js
+++ schema/userSchema.js
@@ -1,11 +1,13 @@
-import Joi from 'joi';
+// import Joi from 'joi';
+const Joi = require('joi')
+    .extend(require('@joi/date'));
 
 const userSchema = Joi.object({
     first_name: Joi.string().required(),
     last_name: Joi.string().required(),
     user_name: Joi.string().required(),
-    email: Joi.string(),
-    birthday: Joi.date().required(),
+    email: Joi.string().email().required(),
+    birthday: Joi.date().format('DD-MM-YYYY').required(),
     contact_phone: Joi.array().items(
         Joi.object({
             label: Joi.string().required(),
